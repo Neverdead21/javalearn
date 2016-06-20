46e14b16b3fb7fb2bb72928b2c03e16b7f1a0e5d
Neverdead21 <andrey.budylin@icloud.com>
2016-06-21 01:26:24 +0800

    adds logic

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,9 @@
 class Calculate{
 	public static void main(String[] args){
 		System.out.println("Calculate in progress...");
+		int first = Integer.valueOf(args[0]);
+		int second = Integer.valueOf(args[1]);
+		int sum = first-second;
+		System.out.println("Sum is "+sum);
 	}
 }
